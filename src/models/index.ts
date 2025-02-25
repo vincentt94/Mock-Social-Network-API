d0a8df0f4494b9ed68a193e0220023cb2ac844d5
vincentt94 <vincent.h.thao@gmail.com>
2025-02-24 20:33:38 -0600

    added userRoutes, thoughtsRoutss, index.ts for api folder, index.ts for routes folder .. need to add controllers and update  into thoughtsRoutes and userRoutes

---
+++ src/models/index.ts
@@ -0,0 +1,4 @@
+import Thoughts from './thought.js';
+import Users from './user.js';
+
+export {Thoughts, Users};
---
+++ src/routes/api/index.ts
@@ -0,0 +1,13 @@
+import {Router} from 'express';
+const router = Router();
+//  import routes from users
+// import userRoutes from './userRoutes.js';
+
+// import routes from thoughts
+// import thoughtsRoutes from './thoughtsRoutes.js';
+
+
+// router.use('/users', userRoutes);
+// router.use('./thoughts', thoughtsRoutes);
+
+export default router;
